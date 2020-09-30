adf11233c4623d5225154c471ff759dd0b857603
Michelle Moon <robbins.michelle@ymail.com>
2020-09-30 17:19:27 -0500

    intial psuedo code in js

---
+++ Develop/script.js
@@ -0,0 +1,13 @@
+// elements to keep track of : 
+// time, inputbox, button, date at top of screen
+// time- past present, future -changes color
+// input- stores text of event
+// button- saves to local storage if page is refreshed
+// display dtae at top of screen
+
+// how do the elemtns change?
+// time- color changes when its past present or future
+// type text start empty
+// on click button saves event
+// date will change per day
+
