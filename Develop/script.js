2d5817dae4725cf5aad66c93a476818f3b3a9978
Michelle Moon <robbins.michelle@ymail.com>
2020-10-01 19:25:09 -0500

    working colors and start of button

--- Develop/script.js
+++ Develop/script.js
@@ -2,42 +2,59 @@
 
 var timeOfDayEl = $(".row");
 var textInputEl = $(".description");
- var bttn = $(".saveBtn");
-
+var bttn = $(".saveBtn");
 var date = $("#currentDay");
+var currentTime = parseInt(moment().format('h'));
 
+// display current day
 date.text(moment().format('dddd, MMM Do'));
 
-// keep track of past, present, and future time
-function color(){
-     var displayColor = moment();
+// // keep track of past, present, and future time by color coding
 
- if (moment()){
-    timeOfDayEl.style("background-color", "red");
-  }
+function color() {
+    
+    textInputEl.each(function( index ) {
+        var timeText = parseInt($(this).attr("id"));  
+        
+        if (timeText === currentTime) {
+            $(this).css("background-color", "red");
+        }
 
-else if(){
-    timeOfDayEl.style("background-color", "yellow");
-}
+        else if (timeText > currentTime) {
+            $(this).css("background-color", "yellow");
+        }
 
-else{
-        timeOfDayEl.style("background-color", "gray");
+        else {
+            $(this).css("background-color", "gray");
+        }
+
+}); 
 };
+color();
 
-// write text and click button to save into local storage
+// on click button get text current row and save to local storage
+bttn.on("click", function(event){
+    
+var currentRow = $(event.target).parent(); 
+var textArea = currentRow.find("textarea");
+var text = textArea.val();
+// // textArea.each(fucntion()){
+// //     var eventText= localStorage.setItem("text" , JSON.stringfiy(text));
+//  }
 
 
-// on click button saves event
-// bttn.on("click", function(event){
 
-// var eventText = localStorage.setItem("text", )
 // });
+// //  Get from local storage
+// function store(){
+//     var storedText = JSON.parse(localStorage.getItem("text"));
+
+// }
+
+// store();
 
 
 
-
-
-// how do the elements change?
 
 
 
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Work-Day-Scheduler
 *   Set up HTML to match CSS
+*   Researched moment to display current day 
+*   Made a function to display the past hours as gray, present hour as red, and future hours as yellow using moment
 *   
